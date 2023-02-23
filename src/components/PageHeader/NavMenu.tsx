c7ec38b7b10a0e63f93ae4c0a2c5e738f2f154f4
Sebastian Rosloniec <sr@ebitlabs.com>
2023-02-23 22:44:39 +0000

    refactor(NavMenu): parameterize menu items; extract MenuItem

--- src/components/PageHeader/NavMenu.tsx
+++ src/components/PageHeader/NavMenu.tsx
@@ -1,3 +1,125 @@
+type ItemProps = {
+	href: string;
+	label: string;
+	subItems?: ItemProps[];
+	isHome?: boolean;
+};
+
+const menuItems: ItemProps[] = [
+	{
+		isHome: true,
+		href: "/",
+		label: "Home",
+	},
+	{
+		href: "/category/the-dirt-shed-show",
+		label: "Dirt Shed Show",
+	},
+	{
+		href: "/category/how-to",
+		label: "How To",
+	},
+	{
+		href: "/category/mtb-maintenance",
+		label: "MTB Maintenance",
+	},
+	{
+		href: "/category/martyn-ashton",
+		label: "Martyn Ashton",
+	},
+	{
+		href: "/category/ask-gmbn",
+		label: "Ask GMBN",
+	},
+	{
+		href: "/category/features",
+		label: "Features",
+	},
+	{
+		href: "/category/top-10s",
+		label: "Top 10s",
+	},
+	{
+		href: "/category/gmbn-tech",
+		label: "GMBN Tech",
+	},
+	{
+		href: "/presenters",
+		label: "Presenters",
+		subItems: [
+			{
+				href: "/presenters/martyn-ashton",
+				label: "Martyn Ashton",
+			},
+			{
+				href: "/presenters/neil-donoghue",
+				label: "Neil Donoghue",
+			},
+			{
+				href: "/presenters/blake-samson",
+				label: "Blake Samson",
+			},
+			{
+				href: "/presenters/andrew-dodd",
+				label: "Andrew Dodd",
+			},
+		],
+	},
+];
+
+const makeClassName = (...chunks: (string | false | undefined)[]) =>
+	chunks.filter((x) => x).join(" ");
+
+export const MenuItem: React.FunctionComponent<ItemProps> = ({
+	href,
+	label,
+	subItems,
+	isHome,
+}) => (
+	<li
+		key={href}
+		className={makeClassName(
+			"dropdown__list",
+			isHome && "dropdown__list--home",
+			subItems &&
+				"dropdown__list dropdown__list--submenu is-dropdown-submenu-parent opens-right"
+		)}
+		role="menuitem"
+		// aria-haspopup="true"
+	>
+		<a
+			className={makeClassName(
+				"dropdown__list__item",
+				subItems && "dropdown__list__item--submenu"
+			)}
+			{...{ href }}
+		>
+			{label}
+		</a>
+
+		{subItems && (
+			<ul
+				className="menu dropdown__list__submenu is-dropdown-submenu submenu first-sub vertical"
+				data-submenu
+				role="menu"
+			>
+				{subItems?.map(({ href, label }) => (
+					<li
+						key={href}
+						className="dropdown__list__submenulist is-submenu-item is-dropdown-submenu-item"
+						role="menuitem"
+						// aria-haspopup="true"
+					>
+						<a className="dropdown__list__submenulist__item" {...{ href }}>
+							{label}
+						</a>
+					</li>
+				))}
+			</ul>
+		)}
+	</li>
+);
+
 type Props = {
 	isMenuOpen: boolean;
 };
@@ -21,182 +143,9 @@ export const NavMenu: React.FunctionComponent<Props> = ({
 								data-dropdown-menu="r2ecvi-dropdown-menu"
 								role="menubar"
 							>
-								<li
-									className="dropdown__list dropdown__list--home"
-									role="menuitem"
-								>
-									{/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-									<a className="dropdown__list__item" href="/">
-										Home
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a
-										className="dropdown__list__item"
-										href="/category/the-dirt-shed-show"
-									>
-										Dirt Shed Show
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a className="dropdown__list__item" href="/category/how-to">
-										How To
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a
-										className="dropdown__list__item"
-										href="/category/mtb-maintenance"
-									>
-										MTB Maintenance
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a
-										className="dropdown__list__item"
-										href="/category/martyn-ashton"
-									>
-										Martyn Ashton
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a className="dropdown__list__item" href="/category/ask-gmbn">
-										Ask GMBN
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a className="dropdown__list__item" href="/category/features">
-										Features
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a className="dropdown__list__item" href="/category/top-10s">
-										Top 10s
-									</a>
-								</li>
-								<li className="dropdown__list" role="menuitem">
-									<a
-										className="dropdown__list__item"
-										href="/category/gmbn-tech"
-									>
-										GMBN Tech
-									</a>
-								</li>
-								<li
-									className="dropdown__list dropdown__list--mobile"
-									role="menuitem"
-								>
-									<a className="dropdown__list__item" href="/presenters">
-										Presenters
-									</a>
-								</li>
-								<li
-									className="dropdown__list dropdown__list--mobile"
-									role="menuitem"
-								>
-									<a
-										className="dropdown__list__item"
-										href="/presenters/martyn-ashton"
-									>
-										Martyn Ashton
-									</a>
-								</li>
-								<li
-									className="dropdown__list dropdown__list--mobile"
-									role="menuitem"
-								>
-									<a
-										className="dropdown__list__item"
-										href="/presenters/neil-donoghue"
-									>
-										Neil Donoghue
-									</a>
-								</li>
-								<li
-									className="dropdown__list dropdown__list--mobile"
-									role="menuitem"
-								>
-									<a
-										className="dropdown__list__item"
-										href="/presenters/blake-samson"
-									>
-										Blake Samson
-									</a>
-								</li>
-								<li
-									className="dropdown__list dropdown__list--mobile"
-									role="menuitem"
-								>
-									<a
-										className="dropdown__list__item"
-										href="/presenters/andrew-dodd"
-									>
-										Andrew Dodd{" "}
-									</a>
-								</li>
-								<li
-									className="dropdown__list dropdown__list--submenu is-dropdown-submenu-parent opens-right"
-									role="menuitem"
-									aria-haspopup="true"
-									aria-label="Presenters"
-								>
-									<a
-										className="dropdown__list__item dropdown__list__item--submenu"
-										href="/presenters"
-									>
-										Presenters
-									</a>
-									<ul
-										className="menu dropdown__list__submenu is-dropdown-submenu submenu first-sub vertical"
-										data-submenu
-										role="menu"
-									>
-										<li
-											className="dropdown__list__submenulist is-submenu-item is-dropdown-submenu-item"
-											role="menuitem"
-										>
-											<a
-												className="dropdown__list__submenulist__item"
-												href="/presenters/martyn-ashton"
-											>
-												Martyn Ashton
-											</a>
-										</li>
-										<li
-											className="dropdown__list__submenulist is-submenu-item is-dropdown-submenu-item"
-											role="menuitem"
-										>
-											<a
-												className="dropdown__list__submenulist__item"
-												href="/presenters/neil-donoghue"
-											>
-												Neil Donoghue
-											</a>
-										</li>
-										<li
-											className="dropdown__list__submenulist is-submenu-item is-dropdown-submenu-item"
-											role="menuitem"
-										>
-											<a
-												className="dropdown__list__submenulist__item"
-												href="/presenters/blake-samson"
-											>
-												Blake Samson
-											</a>
-										</li>
-										<li
-											className="dropdown__list__submenulist is-submenu-item is-dropdown-submenu-item"
-											role="menuitem"
-										>
-											<a
-												className="dropdown__list__submenulist__item"
-												href="/presenters/andrew-dodd"
-											>
-												Andrew Dodd{" "}
-											</a>
-										</li>
-									</ul>
-								</li>
+								{menuItems.map((itemProps) => (
+									<MenuItem key={itemProps.href} {...itemProps} />
+								))}
 							</ul>
 						</div>
 					</div>
