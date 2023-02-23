ee451e2e5436679261836d45cdaa7fefa64e0168
Sebastian Rosloniec <sr@ebitlabs.com>
2023-02-23 20:26:57 +0000

    create PageHeader

---
+++ src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,3 @@
+export const PageHeader: React.FunctionComponent = () => {
+	return <header></header>;
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "src/styles/globals.css";
+import "src/styles/main-original.css";
+
+import { PageHeader } from "src/components/PageHeader";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<PageHeader />
+			<Component {...pageProps} />;
+		</>
+	);
 }
