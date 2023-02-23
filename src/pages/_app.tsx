7a24c2645b89156c9f5c178c75fd1b1161452d2e
Sebastian Rosloniec <sr@ebitlabs.com>
2023-02-23 20:35:53 +0000

    fix body overflow-y glitch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
 import type { AppProps } from "next/app";
 
-import "src/styles/globals.css";
 import "src/styles/main-original.css";
 
 import { PageHeader } from "src/components/PageHeader";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<div id="container">
 			<PageHeader />
 			<Component {...pageProps} />;
-		</>
+		</div>
 	);
 }
