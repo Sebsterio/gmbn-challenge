6223aeb9da54a3a14c023c6e22e1f7cc1974efc9
Sebastian Rosloniec <sr@ebitlabs.com>
2023-02-24 15:12:41 +0000

    feat(header): implement fixed position

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 
+// fallback styles for legacy elements
 import "src/styles/main-original.css";
 
 import { PageHeader } from "src/components/PageHeader";
@@ -7,8 +8,20 @@ import { PageHeader } from "src/components/PageHeader";
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<div id="container">
-			<PageHeader />
-			<Component {...pageProps} />;
+			<div
+				style={{
+					height: "100vh",
+					display: "flex",
+					flexFlow: "column",
+					flexWrap: "nowrap",
+				}}
+			>
+				<PageHeader />
+
+				<div style={{ flex: 1, overflowY: "auto" }}>
+					<Component {...pageProps} />;
+				</div>
+			</div>
 		</div>
 	);
 }
